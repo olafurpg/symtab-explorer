65638cdd80d51c8ac36c3d6652690782dbc12899
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-11-16 17:34:44 +0100

    Write basic app.

--- explorer/src/main/scala/explorer/Main.scala
+++ explorer/src/main/scala/explorer/Main.scala
@@ -5,17 +5,27 @@ import scala.meta._
 import scala.meta.internal.symtab._
 
 object Main {
-    def bootClasspath: Option[Classpath] = sys.props.collectFirst {
-        case (k, v) if k.endsWith(".boot.class.path") => Classpath(v)
-      }
+  def bootClasspath: Option[Classpath] = sys.props.collectFirst {
+    case (k, v) if k.endsWith(".boot.class.path") => Classpath(v)
+  }
   def main(args: Array[String]): Unit = {
-    val classpath = Classpath(args.mkString(java.io.File.pathSeparator))
-    val symtab = GlobalSymbolTable(classpath ++ bootClasspath.get)
-    val symbols = new Symtab {
-        def info(symbol: Symbol): Option[SymbolInformation] = {
+    args.toList match {
+      case jars :: symbol :: Nil =>
+        val classpath = Classpath(args.mkString(java.io.File.pathSeparator))
+        val symtab = GlobalSymbolTable(classpath ++ bootClasspath.get)
+        val symbols = new Symtab {
+          def info(symbol: Symbol): Option[SymbolInformation] = {
             symtab.info(symbol.value).map(i => new SymbolInformation(i)(this))
+          }
         }
+        symbols.info(Symbol(symbol)) match {
+          case Some(info) =>
+            println(info)
+          case None =>
+            println(s"not found: $symbol")
+        }
+      case els =>
+        sys.error(s"Expected <classpath> <symbol>. Obtained $els")
     }
-    println(symbols.info(Symbol("scala/Int#")))
   }
 }
