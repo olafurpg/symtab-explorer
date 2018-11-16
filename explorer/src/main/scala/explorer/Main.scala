fbbc4adc4f74b5a34ab67fa669bfd43f9ebb387e
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-11-16 17:32:36 +0100

    WIP

--- explorer/src/main/scala/explorer/Main.scala
+++ explorer/src/main/scala/explorer/Main.scala
@@ -1,10 +1,21 @@
-package explorer
+package scalafix.explorer
 
 import scalafix.v1._
 import scala.meta._
+import scala.meta.internal.symtab._
 
 object Main {
+    def bootClasspath: Option[Classpath] = sys.props.collectFirst {
+        case (k, v) if k.endsWith(".boot.class.path") => Classpath(v)
+      }
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+    val classpath = Classpath(args.mkString(java.io.File.pathSeparator))
+    val symtab = GlobalSymbolTable(classpath ++ bootClasspath.get)
+    val symbols = new Symtab {
+        def info(symbol: Symbol): Option[SymbolInformation] = {
+            symtab.info(symbol.value).map(i => new SymbolInformation(i)(this))
+        }
+    }
+    println(symbols.info(Symbol("scala/Int#")))
   }
 }
